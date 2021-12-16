bb29c36e976ef28a897f1a8a2d57e16e79eaa73a
Andrew Toner <git@andytoner.com>
2021-12-15 21:54:58 -0800

    Day08 tests

---
+++ pkg/day08/day08_test.go
@@ -0,0 +1,45 @@
+package day08
+
+import "testing"
+
+func TestPart1(t *testing.T) {
+	type args struct {
+		fileName string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "Sample", args: args{fileName: "../../input/day08/day08_sample.txt"}, want: 26},
+		{name: "Actual", args: args{fileName: "../../input/day08/day08.txt"}, want: 321},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Part1(tt.args.fileName); got != tt.want {
+				t.Errorf("Part1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPart2(t *testing.T) {
+	type args struct {
+		fileName string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "Sample", args: args{fileName: "../../input/day08/day08_sample.txt"}, want: 61229},
+		{name: "Actual", args: args{fileName: "../../input/day08/day08.txt"}, want: 1028926},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Part2(tt.args.fileName); got != tt.want {
+				t.Errorf("Part2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

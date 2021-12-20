0c052f1e4d9b9cdff90c4d7f9322c403e1fc0d69
Andrew Toner <git@andytoner.com>
2021-12-19 22:08:31 -0800

    Day11 Part 1

--- cmd/aoc2021/main.go
+++ cmd/aoc2021/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"andytoner.com/aoc2021/pkg/day10"
+	"andytoner.com/aoc2021/pkg/day11"
 	"fmt"
 )
 
 func main() {
-	fmt.Printf("%d\n", day10.Part2("./input/day10/day10_sample.txt"))
+	fmt.Printf("%d\n", day11.Part1("./input/day11/day11.txt"))
 }

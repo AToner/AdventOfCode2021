8a0b420fea72a5fdf90467fce4f8af6aefd5492e
Andrew Toner <git@andytoner.com>
2021-12-22 20:22:38 -0800

    Day12 Part1

--- cmd/aoc2021/main.go
+++ cmd/aoc2021/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"andytoner.com/aoc2021/pkg/day11"
+	"andytoner.com/aoc2021/pkg/day12"
 	"fmt"
 )
 
 func main() {
-	fmt.Printf("%d\n", day11.Part1("./input/day11/day11.txt"))
+	fmt.Printf("%d\n", day12.Part1("./input/day12/day12.txt"))
 }
---
+++ pkg/day12/day12_test.go
@@ -0,0 +1,24 @@
+package day12
+
+import "testing"
+
+func TestPart1(t *testing.T) {
+	type args struct {
+		fileName string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "Sample", args: args{fileName: "../../input/day12/day12_sample.txt"}, want: 226},
+		{name: "Actual", args: args{fileName: "../../input/day12/day12.txt"}, want: 5252},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Part1(tt.args.fileName); got != tt.want {
+				t.Errorf("Part1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

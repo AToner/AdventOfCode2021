e16c1e0d44f59ca0c4e43b663c99e991997d508f
Andrew Toner <git@andytoner.com>
2022-01-10 21:03:09 -0800

    Day17 Part 2

--- cmd/aoc2021/main.go
+++ cmd/aoc2021/main.go
@@ -9,6 +9,6 @@ import (
 func main() {
 	start := time.Now()
 	fmt.Println(start)
-	fmt.Printf("%d\n", day17.Part1("./input/day17/day17.txt"))
+	fmt.Printf("%d\n", day17.Part2("./input/day17/day17.txt"))
 	fmt.Println(time.Now().Sub(start))
 }
